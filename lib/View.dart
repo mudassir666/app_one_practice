23369af87f1e66f2bc1104c067a640ce5ec4e440
mudassir666 <mudassir666666666@gmail.com>
2021-07-23 02:06:39 +0500

    Part_2_Completed

--- lib/View.dart
+++ lib/View.dart
@@ -9,6 +9,54 @@ class View extends StatefulWidget {
 }
 
 class _ViewState extends State<View> {
+ 
+  late String ration = "50";
+
+  String aMath(String a) {
+    // var grade = a;
+    switch (a) {
+      case "A":
+        {
+          print("Excellent");
+          return ration = "10";
+        }
+      //break;
+
+      case "B":
+        {
+          print("Good");
+          return ration = "20";
+        }
+      //break;
+
+      case "C":
+        {
+          print("Fair");
+          return ration = "30";
+        }
+      //break;
+
+      case "D":
+        {
+          print("Poor");
+          return ration = "40";
+        }
+      //break;
+
+      default:
+        {
+          print("Invalid choice");
+          return ration = "60";
+        }
+      //break;
+    }
+  }
+
+  TextEditingController _name = TextEditingController();
+  TextEditingController _choice = TextEditingController();
+  List<String> save = [];
+  late String word;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,6 +70,86 @@ class _ViewState extends State<View> {
               },
               icon: Icon(Icons.exit_to_app))
         ],
+      ),
+      body: Center(
+        child: Container(
+          child: SingleChildScrollView(
+            child: Card(
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: _name,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintText: "Enter Name",
+                      labelText: "NAME",
+                      icon: Icon(Icons.person),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    controller: _choice,
+                    keyboardType: TextInputType.text,
+                    decoration: InputDecoration(
+                      hintText: "Choice a number",
+                      labelText: "CHOICE",
+                      icon: Icon(Icons.zoom_out),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(ration),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton.icon(
+                      onPressed: () {
+                        // var change = _choice.text.toString();
+                        aMath(_choice.text.toString());
+                        setState(() {
+                          
+                        });
+                      },
+                      icon: Icon(Icons.zoom_in_outlined),
+                      label: Text("Choice"))
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          word = _name.text;
+          save.add(word);
+          setState(() {});
+          print(save);
+        },
+        child: Icon(Icons.add),
+      ),
+      drawer: Drawer(
+        child: save.length != 0
+            ? ListView.builder(
+                // padding: EdgeInsets.zero,
+                // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(
+                      save[index],
+                      style:
+                          TextStyle(fontStyle: FontStyle.italic, fontSize: 18),
+                    ),
+                    tileColor: Colors.amber[100],
+                  );
+                },
+                itemCount: save.length,
+              )
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
     );
   }
